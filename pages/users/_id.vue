61e6bfc2847de8339a13cfbda2ef28fdeec3015e
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-07-08 16:47:48 +0100

    Video - 05

---
+++ pages/users/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="title">A single parsclick.net user {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$route)
+  },
+}
+</script>
--- pages/users/amir.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1 class="title">Amir</h1>
-  </div>
-</template>
