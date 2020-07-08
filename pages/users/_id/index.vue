f5af5fcf266e50c7ad1d3e6742f542c1ce10b6fb
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-07-08 17:50:47 +0100

    Video - 07

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h1 class="title">A single parsclick.net user {{ $route.params.id }}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	/**
+	 * Route Validation
+	 */
+	validate(data) {
+		console.log(data)
+		return /^\d+$/.test(data.params.id)
+	},
+}
+</script>
