8457a1b008833ed13c7a32f1a13c8e5a515e55ae
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-07-08 16:08:11 +0100

    Video - 03

---
+++ pages/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt-content :document="index" />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const index = await $content(params.slug).fetch()
+
+    await console.log(params)
+
+    return { index }
+  },
+}
+</script>
